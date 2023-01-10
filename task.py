ca411fd6865e266054e885efc35f86a613b43eda
kazukishirasu <s19C1057FA@s.chibakoudai.jp>
2023-01-10 13:16:16 +0900

    update

--- task.py
+++ task.py
@@ -2,87 +2,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-score = ["true", "false", "false", "true", "true"]
-# score = ["true", "true", "true", "true", "true"]
+score = [["true", "false", "false", "true", "true"], 
+         ["true", "true", "true", "true", "true"]]
 x = np.arange(0, 1.01, step=0.01)
 y = []
 
-if score[0] == "true":
-    const = 1 / (sum(np.arange(0, 1.01, step=0.01) * 1/101))
-    t_y = const * np.arange(0, 1.01, step=0.01) * 1/101
-    flag = True
-    y.append(t_y)
-elif score[0] == "false":
-    const = 1 / (sum((1 - np.arange(0, 1.01, step=0.01)) * 1/101))
-    f_y = const * (1 - np.arange(0, 1.01, step=0.01)) * 1/101
-    flag = False
-    y.append(f_y)
-
-for i in score[1:]:
-    if i == "true":
-        if flag:
-            const = 1/(sum(np.arange(0, 1.01, step=0.01) * t_y))
-            t_y = const * np.arange(0, 1.01, step=0.01) * t_y
-        else:
-            const = 1/(sum(np.arange(0, 1.01, step=0.01) * f_y))
-            t_y = const * np.arange(0, 1.01, step=0.01) * f_y
+for i in range(len(score)):
+    y.append(np.full_like(x, 1/101))
+    if score[i][0] == "true":
+        const = 1 / (sum(np.arange(0, 1.01, step=0.01) * 1/101))
+        t_y = const * np.arange(0, 1.01, step=0.01) * 1/101
         flag = True
         y.append(t_y)
-    elif i == "false":
-        if flag:
-            const = 1/(sum((1 - np.arange(0, 1.01, step=0.01)) * t_y))
-            f_y = const * (1 - np.arange(0, 1.01, step=0.01)) * t_y
-        else:
-            const = 1/(sum((1 - np.arange(0, 1.01, step=0.01)) * f_y))
-            f_y = const * (1 - np.arange(0, 1.01, step=0.01)) * f_y
+    elif score[i][0] == "false":
+        const = 1 / (sum((1 - np.arange(0, 1.01, step=0.01)) * 1/101))
+        f_y = const * (1 - np.arange(0, 1.01, step=0.01)) * 1/101
         flag = False
         y.append(f_y)
+    for j in score[i][1:]:
+        if j == "true":
+            if flag:
+                const = 1/(sum(np.arange(0, 1.01, step=0.01) * t_y))
+                t_y = const * np.arange(0, 1.01, step=0.01) * t_y
+            else:
+                const = 1/(sum(np.arange(0, 1.01, step=0.01) * f_y))
+                t_y = const * np.arange(0, 1.01, step=0.01) * f_y
+            flag = True
+            y.append(t_y)
+        elif j == "false":
+            if flag:
+                const = 1/(sum((1 - np.arange(0, 1.01, step=0.01)) * t_y))
+                f_y = const * (1 - np.arange(0, 1.01, step=0.01)) * t_y
+            else:
+                const = 1/(sum((1 - np.arange(0, 1.01, step=0.01)) * f_y))
+                f_y = const * (1 - np.arange(0, 1.01, step=0.01)) * f_y
+            flag = False
+            y.append(f_y)
 print(y)
-# fig, ax = plt.subplots(2, 3, sharex="all", sharey="all", tight_layout=True)
 fig, ax = plt.subplots(2, 3, tight_layout=True)
-x0 = [0, 1]
-y0 = [1/101, 1/101]
-ax[0, 0].plot(x0, y0)
-ax[0, 0].set_xlabel("t")
-ax[0, 0].set_ylabel("probability")
-ax[0, 0].set_xlim(0, 1)
-ax[0, 0].set_ylim(0, 0.1)
-ax[0, 0].minorticks_on()
-ax[0, 0].grid(axis="x")
-ax[0, 0].grid(which = "both", axis="y")
-
-ax[0, 1].plot(x, y[0])
-ax[0, 1].set_xlim(0, 1)
-ax[0, 1].set_ylim(0, 0.1)
-ax[0, 1].minorticks_on()
-ax[0, 1].grid(axis="x")
-ax[0, 1].grid(which = "both", axis="y")
-
-ax[0, 2].plot(x, y[1])
-ax[0, 2].set_xlim(0, 1)
-ax[0, 2].set_ylim(0, 0.1)
-ax[0, 2].minorticks_on()
-ax[0, 2].grid(axis="x")
-ax[0, 2].grid(which = "both", axis="y")
-
-ax[1, 0].plot(x, y[2])
-ax[1, 0].set_xlim(0, 1)
-ax[1, 0].set_ylim(0, 0.1)
-ax[1, 0].minorticks_on()
-ax[1, 0].grid(axis="x")
-ax[1, 0].grid(which = "both", axis="y")
-
-ax[1, 1].plot(x, y[3])
-ax[1, 1].set_xlim(0, 1)
-ax[1, 1].set_ylim(0, 0.1)
-ax[1, 1].minorticks_on()
-ax[1, 1].grid(axis="x")
-ax[1, 1].grid(which = "both", axis="y")
-
-ax[1, 2].plot(x, y[4])
-ax[1, 2].set_xlim(0, 1)
-ax[1, 2].set_ylim(0, 0.1)
-ax[1, 2].minorticks_on()
-ax[1, 2].grid(axis="x")
-ax[1, 2].grid(which = "both", axis="y")
+ax = ax.ravel()
+for num in range(int(len(y) / 2)):
+    print(num)
+    ax[num].plot(x, y[num])
+    ax[num].plot(x, y[num + 6])
+    ax[num].set_xlim(0, 1)
+    ax[num].set_ylim(0, 0.1)
+    ax[num].minorticks_on()
+    ax[num].grid(axis="x")
+    ax[num].grid(which = "both", axis="y")
 plt.show()
